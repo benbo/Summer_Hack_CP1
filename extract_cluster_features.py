59df4bca2f4ef1237fe8227ac073a75038acd00a
Nicholas Gisolfi <ngisolfi@lot1.int.autonlab.org>
2016-07-21 15:56:03 -0400

    Added temporal feature extraction function

--- extract_cluster_features.py
+++ extract_cluster_features.py
@@ -1,13 +1,14 @@
 import os
 import json
-from featurize_clusters import TextFeaturizer
+#from featurize_clusters import TextFeaturizer
 import numpy as np
-from itertools import chain
-from pyproj import Proj,transform
+#from itertools import chain
+#from pyproj import Proj,transform
 
 
-INPROJ = Proj(init='epsg:4326')
-OUTPROJ = Proj(init='epsg:3857')
+#INPROJ = Proj(init='epsg:4326')
+#OUTPROJ = Proj(init='epsg:3857')
+MV = 'None' # handle missing values this way
 
 def cluster_text_summary(textfeaturizer,text):
     '''
@@ -67,6 +68,56 @@ def cluster_location_summary(lattice_cluster):
 
     return np.hstack((missing_lat_proportion,np.squeeze(centroid_cluster),area_cluster,disp_metric))
 
+def temporal_summary(lattice_cluster):
+	epoch_timestamps=list()
+	posttime = gen_get( ['posttime'] , lattice_cluster )#Following is unique to posttime feature
+	for item in posttime:
+		time_elements= list()
+		entities = item.split()#item.split() is of form ['Monday,','June','26,','2016','6:52','PM']
+		try:
+			hm_split = entities[4].split(':')
+			time_elements.extend( [entities[2].strip(',')] )#day
+			time_elements.extend( [month2int(entities[1])] )#month
+			time_elements.extend( [entities[3]] )#year
+			time_elements.extend( [hm_split[0]] )#hour
+			time_elements.extend( [hm_split[1]] )#minute
+			time_elements.extend( [0] ) #second...missing from tags but for completeness, set 0
+			time_elements = map( int , time_elements )#Mix of strings/ints to all ints
+
+			#Convert to military time
+			if entities[5]=='PM' and time_elements[3]<12:
+				time_elements[3] = time_elements[3]+12
+			elif entities[5]=='AM' and time_elements[3]==12:
+				time_elements[3] = 0
+
+			#Convert to unix epoch time
+			epoch_timestamps.extend( [make_timestamp(time_elements)] )
+		except:
+			epoch_timestamps.extend( [MV] )
+
+	#String missing values and sort remaining
+	sorted_epoch_posttimes = filter( lambda a: a!='None', epoch_timestamps )
+	sorted_epoch_posttimes.sort()
+
+	#1hour = 3600, 1day = 86400, 1week = 604800, 1month = 2629743, 1year = 31556926
+	num_posts_in_24hr_windows = get_sliding_window_count( sorted_epoch_posttimes , 86400 )
+	#for example, num_posts_in_1hr_window = get_sliding_window_count(sorted_epoch_posttimes,3600)
+
+	max_posts_per_24hr_period = max( num_posts_in_24hr_windows )
+	min_posts_per_24hr_period = min( num_posts_in_24hr_windows )
+	avg_posts_per_24hr_period = np.mean( num_posts_in_24hr_windows )
+	med_posts_per_24hr_period = np.median( num_posts_in_24hr_windows )
+	std_posts_per_24hr_period = np.std( num_posts_in_24hr_windows )
+
+	cluster_time_summary = [ max_posts_per_24hr_period ,
+						min_posts_per_24hr_period ,
+						avg_posts_per_24hr_period ,
+						med_posts_per_24hr_period ,
+						std_posts_per_24hr_period ]
+
+	return cluster_time_summary
+
+
 
 def get_age(cluster):
     for item in cluster:
@@ -89,3 +140,131 @@ def get_coord_prob(cluster):
                 prob = map(float,(d['probability'] for d in item['extractions']['lattice-location']['results'] if d['context']['city']['centroid_lat'] is not None))
                 if prob:
                     yield prob[0] ###MODIFY to extract the prob needed, according to how locations are aggregated
+
+
+
+
+#Expand the json entries to a list of all entries of the same tag
+def gen_get( keys , raw_data ):
+	feature = list()
+	for record in raw_data:
+		if keys[0] in record['extractions']:
+			if len(keys)==1:
+				try:
+					candidate = [str(record['extractions'][keys[0]]['results'][0])]
+					if( candidate[0].find('html')<0 ): #Some fields contain html for the whole scraped page
+						feature.extend(candidate)
+				except:
+					feature.extend([MV])
+			elif len(keys)==2:
+				try:
+					candidate = [str(record['extractions'][keys[0]]['results'][0][keys[1]])]
+					if( candidate[0].find('html')<0 ):
+						feature.extend(candidate)
+				except:
+					feature.extend([MV])
+			elif len(keys)==3:
+				try:
+					candidate = [str(record['extractions'][keys[0]]['results'][0][keys[1]][keys[2]])]
+					if( candidate[0].find('html')<0 ):
+						feature.extend(candidate)
+				except:
+					feature.extend([MV])
+			elif len(keys)==4:
+				try:
+					candidate = [str(record['extractions'][keys[0]]['results'][0][keys[1]][keys[2]][keys[3]])]
+					if( candidate[0].find('html')<0 ):
+						feature.extend(candidate)
+				except:
+					feature.extend([MV])
+			else:
+				feature.extend([MV])
+		else:
+			feature.extend([MV])
+	return feature
+
+
+#Convert the month name to a month integer
+def month2int( monthstr ):
+	if monthstr == 'January':
+		return 0 #zero indexed, since months represent offset from Jan. 1
+	elif monthstr == 'February':
+		return 1
+	elif monthstr == 'March':
+		return 2
+	elif monthstr == 'April':
+		return 3
+	elif monthstr == 'May':
+		return 4
+	elif monthstr == 'June':
+		return 5
+	elif monthstr == 'July':
+		return 6
+	elif monthstr == 'August':
+		return 7
+	elif monthstr == 'September':
+		return 8
+	elif monthstr == 'October':
+		return 9
+	elif monthstr == 'November':
+		return 10
+	elif monthstr == 'December':
+		return 11
+	else:
+		return 0 #If the month is missing for some reason, assume January
+
+
+# Parse feature and create a numerical epoch timestamp
+# Input looks like [ <day> , <month> , <year> , <hour> , <minute> , <second> ]
+# All elements should be integer type
+def make_timestamp( time_entity_list ):
+	
+	day = time_entity_list[0]
+	month = time_entity_list[1]
+	year = time_entity_list[2]
+	hour = time_entity_list[3]
+	minute = time_entity_list[4]
+	second = time_entity_list[5]
+
+	#Converting all units of time to seconds, i.e. there are 3600 seconds in an hour
+	timestamp = hour*3600 + day*86400 + month*2629743 + year*31556926 + minute*60 + second
+	return timestamp
+
+
+
+# Find distances, d,  between each sequential pair of elements in a list, l
+# i.e. gives you [ d(l[1],l[2]) , d(l[2],l[3]) , d(l[3],l[4]) , d(l[4],l[5]) , ... ]
+def get_sequential_pairwise_distance( myList ):
+	distances = list()
+	for i in range(0,len(myList)-1):
+		distances.extend([myList[i+1]-myList[i]])
+	return distances
+
+
+# Find sequential pairwise distances, then fill windows to see how many occur
+# within a given period of time
+def get_sliding_window_count( myList , windowSize ):
+	element_distances = get_sequential_pairwise_distance( myList )
+	window_count = list()
+	for i in range(0,len(element_distances)-1):
+		j=1
+		span = element_distances[i]
+		while span<windowSize and (i+j+1)<len(element_distances):
+			j+=1
+			span += element_distances[i+j]
+		window_count.extend([j])
+	return window_count
+
+
+
+#***************************************************
+# Uncomment for quick test of temporal summary
+#**************************************************
+#import random
+#jsonfilename2 = '/zfsauton/project/public/memex/lattice/example_1000.json'
+#with open(jsonfilename2, 'r') as infile:
+	#data_lattice = [json.loads(line) for line in infile ]
+#cluster = random.sample(data_lattice,1000)
+#summary = temporal_summary( cluster )
+#print 'max, min, avg, med, std'
+#print summary
